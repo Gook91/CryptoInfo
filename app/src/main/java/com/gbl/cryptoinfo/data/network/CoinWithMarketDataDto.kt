0eba38cb5c96379136037593c99ee11e0bf3d784
Nikolai Guk <gookne@gmail.com>
2024-08-18 17:39:51 +0300

    Fix CoinWithMarketDataDto
    Added serialized names to price and price percentage to load data. Without this gson couldn't serialized those values.

--- app/src/main/java/com/gbl/cryptoinfo/data/network/CoinWithMarketDataDto.kt
+++ app/src/main/java/com/gbl/cryptoinfo/data/network/CoinWithMarketDataDto.kt
@@ -1,12 +1,13 @@
 package com.gbl.cryptoinfo.data.network
 
 import com.gbl.cryptoinfo.entity.CoinWithMarketData
+import com.google.gson.annotations.SerializedName
 
 data class CoinWithMarketDataDto(
     override val id: String,
     override val symbol: String,
     override val name: String,
     override val image: String,
-    override val currentPrice: Float,
-    override val priceChangePercentage24h: Float
+    @SerializedName("current_price") override val currentPrice: Float,
+    @SerializedName("price_change_percentage_24h") override val priceChangePercentage24h: Float
 ) : CoinWithMarketData
