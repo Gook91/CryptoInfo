14ae0c870fd168c012373861ac585c794230e00f
Nikolai Guk <gookne@gmail.com>
2024-08-18 14:17:55 +0300

    Added injection in GetAllCurrenciesUseCase

--- app/src/main/java/com/gbl/cryptoinfo/domain/GetAllCurrenciesUseCase.kt
+++ app/src/main/java/com/gbl/cryptoinfo/domain/GetAllCurrenciesUseCase.kt
@@ -1,7 +1,8 @@
 package com.gbl.cryptoinfo.domain
 
 import com.gbl.cryptoinfo.entity.Currency
+import javax.inject.Inject
 
-class GetAllCurrenciesUseCase {
+class GetAllCurrenciesUseCase @Inject constructor() {
     fun execute(): List<Currency> = Currency.entries
 }
